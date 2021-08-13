948fa761ef292da55114f7b54853371bce637caf
LeraKhodosok <lerakhodosok641@gmail.com>
2021-08-13 11:21:11 +0300

    for Nikita

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,11 @@
-console.log(23);
+const menu = document.querySelector('.menu_block');
+const toggle = document.querySelector('.menu_toggle');
+const menuClose = document.querySelector('.menu_closed');
+
+toggle.addEventListener('click', () => {
+    menu.classList.toggle('active');
+});
+
+menuClose.addEventListener('click', () => {
+    menu.classList.toggle('active');
+})
